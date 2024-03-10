0b8d10a18b878991b931ee186f44bdd6e29747f4
esad260 <98718509+esad260@users.noreply.github.com>
2024-03-10 16:52:21 +0000

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,28 +1,22 @@
 <?php
 
+try {
+    // Update these values for your non-local environment
+    define("HOST", "localhost");
+    define("DBNAME", "forum");
+    define("USER", "databaseuser");
+    define("PASS", "databasepassword");
 
-try{
-  //host
-  define("HOST", "localhost");
+    $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  //dbname
-  define("DBNAME","forum");
-
-  //user
-  define("USER", "root");
-
-  //password
-  define("PASS", "");
-
-  $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-   if($conn == true) {
-     echo "db connection is a success"; 
-  } else{
-    echo "error";
-  }
-} catch(PDOException $Exception ) {
+    // Uncomment the following lines if you want to check the connection status
+     if ($conn) {
+         echo "Database connection is successful!";
+     } else {
+         echo "Error connecting to the database.";
+     }
+} catch (PDOException $Exception) {
     echo $Exception->getMessage();
 }
+?>
